794a74aa55dead9be76916d82285f587a6e054c3
Caleb Carvalho <caleb.carvalho@lme.co.uk>
2014-09-18 13:29:28 +0100

    errorlevel added

--- SedolAcceptanceTests/FitNesse/startFitNesse.bat
+++ SedolAcceptanceTests/FitNesse/startFitNesse.bat
@@ -1,9 +1,9 @@
 @echo off
 
-IF exist ..\bin\Debug (echo ===>>Cool! We assume libraries have been built...We need this for real demo)ELSE (
-echo Attempting to run msBuild cmd.. && msbuild ..\..\MarketDataChecker.sln /t:SedolAcceptanceTests:Rebuild /p:Configuration=Debug /p:Platform="Any CPU") 
+IF exist ..\bin\Debug (echo Cool! We assume libraries have been built...We need this for real demo)ELSE (
+echo Attempting to run msBuild cmd.. && msbuild ..\..\MarketDataChecker.sln /t:SedolAcceptanceTests:Rebuild /p:Configuration=Debug /p:Platform="Any CPU");
 
-if errorlevel 1 echo "====>>>>Build was not executed successfully..PLEASE TRY Opening Solution and build from Visual Studio";
- 
+
+if %errorlevel%==1 (echo Build was not executed successfully..PLEASE TRY Opening Solution and build from Visual Studio)
 
 java -jar fitnesse-standalone.jar -d . -r FitNesseRoot
